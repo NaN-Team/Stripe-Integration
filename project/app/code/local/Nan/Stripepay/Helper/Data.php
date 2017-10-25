bf5a7492e2ebdafd11ff563870149a446282bb39
skaarl <forkmik@gmail.com>
2017-10-25 17:50:33 +0200

    Init project implementation of Stripe payment module for Magento 1.9.

---
+++ project/app/code/local/Nan/Stripepay/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skaarl
+ * Date: 25/10/17
+ * Time: 17.48
+ */ 
+class Nan_Stripepay_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ project/app/code/local/Nan/Stripepay/sql/nan_stripepay_setup/install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skaarl
+ * Date: 25/10/17
+ * Time: 17.48
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
